345fdf684c1399622f083f41f148f1733cdfd904
bo.zeng <603490698@qq.com>
2017-08-09 11:45:32 +0800

    remove test file

--- test.py
+++
@@ -1,3 +0,0 @@
-import urllib.request
-url="https://9.xhcdn.com/key=jc-fP6x5i54Yrd9jW97PDg,end=1501873183,limit=3/data=198.71.235.18-dvp/speed=500k/initial_buffer=2713920/8063699_hd.mp4"    #Note: It's https
-urllib.request.urlretrieve(url, "JpnTeenIdol24.mp4")
